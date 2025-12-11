be05deaa4d6f35f5973b4f494680cfeded52c24e
m02uku <m02ukuzrzr@gmail.com>
2025-12-12 01:44:18 +0900

    DOCKER IS THE BEST!!!!!!!!!!!

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -24,13 +25,30 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         username = "m02uku";
-      in {
+      in
+      {
         packages = {
           hello = pkgs.hello;
         };
-        homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
+
+
+        # devShellsを追加
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hello
+            pkgs.neovim  # プレーンなNeovim
+          ];
+          shellHook = ''
+            echo "開発シェルに入りました！"
+            echo "hello と nvim が使えます"
+          '';
+        };
+
+        legacyPackages = {
+          homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+          };
         };
       }
     );
