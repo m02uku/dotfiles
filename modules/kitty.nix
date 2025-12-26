e9cd3f498ca0f1c5ebae4d4f097d240dfbf80cde
m02uku <m02ukuzrzr@gmail.com>
2025-12-26 23:12:02 +0900

    update: kitty theme is now [[dracula]]

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
+let
+  draculaTheme = builtins.fetchGit {
+    url = "https://github.com/dracula/kitty/";
+    rev = "87717a3f00e3dff0fc10c93f5ff535ea4092de70";
+  };
+in
 {
   programs.kitty = {
     enable = true;
@@ -12,34 +18,15 @@
     settings = {
       # Appearance
       background_opacity = "0.5";
-      background = "#1f1f1f";
-      foreground = "#ff88c7";
-
-      selection_background = "#ff88c7";
-      selection_foreground = "#1e051c";
-
-      cursor = "#ff008c";
-      url_color = "#40e0d1";
-
-      # Font settings
-      bold_font = "auto";
-      italic_font = "auto";
-      bold_italic_font = "auto";
-
-      # Terminal colors
-      color0  = "#000000";  color8  = "#ff7bdc";
-      color1  = "#ff50a2";  color9  = "#ffabd5";
-      color2  = "#ff7fce";  color10 = "#f893f3";
-      color3  = "#d6fff7";  color11 = "#ebb9f7";
-      color4  = "#fb7ac8";  color12 = "#ffb4dd";
-      color5  = "#ff2194";  color13 = "#ffe9be";
-      color6  = "#fc3e9d";  color14 = "#e0c2ff";
-      color7  = "#fdbaff";  color15 = "#ffffff";
+      background_blur = "15";
 
       # Performance / behavior
       enable_audio_bell = false;
-      background_blur = "15";
     };
+
+    extraConfig = ''
+      include ${draculaTheme}/dracula.conf
+    '';
   };
 
   # Ensure the font is available
