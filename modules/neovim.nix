abbfa467d9078bf787a4cb95cab8698aaa400a57
m02uku <m02ukuzrzr@gmail.com>
2025-12-27 00:23:49 +0900

    update: clipboard settings | clipboard now shared with OS

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -4,8 +4,31 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+
+    extraLuaConfig = ''
+      vim. g.mapleader = ' '
+      
+      -- Basic settings
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.expandtab = true
+      vim.opt.shiftwidth = 2
+      vim.opt. tabstop = 2
+      vim.opt.smartindent = true
+      vim.opt.mouse = ""
+      vim.opt.clipboard = "unnamedplus"
+      
+      -- Treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+      
+      -- Telescope keymaps
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    '';
 
     # https://search.nixos.org/packages?channel=25.11&query=vimPlugins
     plugins = with pkgs.vimPlugins; [
@@ -27,30 +50,5 @@
       }
 
     ];
-    
-    extraLuaConfig = ''
-      -- Minimal Neovim configuration
-      vim. g.mapleader = ' '
-      
-      -- Basic settings
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      vim.opt.expandtab = true
-      vim.opt.shiftwidth = 2
-      vim.opt. tabstop = 2
-      vim.opt.smartindent = true
-      vim.opt.mouse = ""
-      
-      -- Treesitter
-      require('nvim-treesitter.configs').setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-      
-      -- Telescope keymaps
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    '';
   };
 }
