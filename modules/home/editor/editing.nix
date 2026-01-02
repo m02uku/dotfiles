733b91f9bea8329a763150b872ecb2d41f518059
m02uku <m02ukuzrzr@gmail.com>
2026-01-03 02:26:53 +0900

    feat: enhance Oil file explorer configuration
    - Add custom columns: icon, permissions, size, mtime
    - Enable skip_confirm_for_simple_edits for better UX
    - Show hidden files by default
    - Improve file explorer functionality

--- modules/home/editor/editing.nix
+++ modules/home/editor/editing.nix
@@ -16,8 +16,13 @@
         enable = true;
         settings = {
           default_file_explorer = true;
+          columns = [ "icon" "permissions" "size" "mtime" ];
           keymaps = {
             "\\" = "actions.close";
+          };
+          skip_confirm_for_simple_edits = true;
+          view_options = {
+            show_hidden = true;
           };
         };
       };
