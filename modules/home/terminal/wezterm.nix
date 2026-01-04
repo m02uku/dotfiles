64a43b1e8f82d62728e896efa53a2f4a928706a8
m02uku <m02ukuzrzr@gmail.com>
2026-01-04 22:36:15 +0900

    Update wezterm config and fix zellij keybinds conflict

--- modules/home/terminal/wezterm.nix
+++ modules/home/terminal/wezterm.nix
@@ -5,24 +5,20 @@
       enable = true;
       extraConfig = ''
         local config = wezterm.config_builder()
-
         -- Font
         config.font = wezterm.font("FiraCode Nerd Font")
         config.font_size = 10.0
-
         -- Appearance
         config.color_scheme = "Dracula"
         config.window_background_opacity = 0.8
         config.window_decorations = "RESIZE"
         config.hide_tab_bar_if_only_one_tab = true
-
         config.macos_window_background_blur = 10
-
         -- Keys
         config.keys = {
           { key = "f", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+          { key = "o", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
         }
-
         return config
       '';
     };
--- modules/home/terminal/zellij.nix
+++ modules/home/terminal/zellij.nix
@@ -4,25 +4,20 @@
     programs.zellij = {
       enable = true;
       settings = {
-        theme = "default";
+        # theme = "default";
         default_shell = "zsh";
-        pane_frames = false;
-        simplified_ui = true;
+        default_mode = "locked"; # デフォルトでLocked Mode
+        # pane_frames = false;
+        # simplified_ui = true;
+        keybinds = {
+          locked = {
+            "Ctrl \\" = "SwitchToMode \"Normal\"";
+          };
+          normal = {
+            "Ctrl \\" = "SwitchToMode \"Locked\"";
+          };
+        };
       };
     };
-
-    home.file.".config/zellij/layouts/dev.kdl".text = ''
-      layout {
-        pane split_direction="vertical" {
-          pane size="75%" focus=true {
-            command "nvim"
-          }
-          pane split_direction="horizontal" size="40%" {
-            pane size="50%"
-            pane size="50%"
-          }
-        }
-      }
-    '';
   };
 }
