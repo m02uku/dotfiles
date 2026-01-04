bbb41b8d428d259e900adb96cded1d0a347b7b15
m02uku <m02ukuzrzr@gmail.com>
2026-01-04 14:58:36 +0900

    Remove redundant docker package, keep colima only

--- modules/home/cli/containers.nix
+++ modules/home/cli/containers.nix
@@ -3,7 +3,6 @@
   flake.modules.homeManager.containers = { pkgs, ... }: {
     home.packages = with pkgs; [
       colima    # Docker on macOS/Linux without Docker Desktop
-      docker    # Docker CLI (colima provides the daemon)
     ];
   };
 }
