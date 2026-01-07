68e5598d7a721d578709a37c4f1dfbf1d915fb63
s0r4d3v <s0r4d3v@gmail.com>
2026-01-08 01:38:09 +0900

    Add typos to python devshell

--- modules/devshells/python/default.nix
+++ modules/devshells/python/default.nix
@@ -1,20 +1,24 @@
 { inputs, ... }:
 {
-  perSystem = { system, ... }: let
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
-  in {
-    devShells.python = pkgs.mkShell {
-      packages = with pkgs; [
-        python313Packages.jupyter
-        uv
-      ];
-      shellHook = ''
-        if [ ! -d venv ]; then
-          uv venv venv
-        fi
-        source venv/bin/activate
-        uv pip install marimo
-      '';
+  perSystem =
+    { system, ... }:
+    let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.python = pkgs.mkShell {
+        packages = with pkgs; [
+          python313Packages.jupyter
+          uv
+          typos
+        ];
+        shellHook = ''
+          if [ ! -d venv ]; then
+            uv venv venv
+          fi
+          source venv/bin/activate
+          uv pip install marimo
+        '';
+      };
     };
-  };
 }
